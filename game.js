704b5eff3a89e54bf952fa698b5f7fec8dfd5b17
S1rShadez <arthure97@hotmail.com>
2017-11-14 09:43:55 +0100

    Lastet opp hoved filene vi starter å jobbe med

---
+++ game.js
@@ -0,0 +1,140 @@
+//Tekstbasert spill
+
+window.onload = function gameOn(){
+
+
+
+
+    //Name
+    var navn = prompt("Choose your charactername");
+    for(var x = 1; x != 0; x++){
+        if(navn === ""){
+            alert("Please choose a name!");
+            navn = prompt("Choose your charactername");
+        }
+        else if(navn === undefined){
+            alert("Please choose a name!");
+            navn = prompt("Choose your charactername");
+        }
+        else{
+            break;
+        }
+    }
+
+
+
+
+    //Sex
+    var kjønnsTest = prompt("Choose between Male and Female");
+    for(var x = 1; x != 0; x++){
+        if(kjønnsTest === undefined){
+            alert("Please choose a sex!");
+            kjønnsTest = prompt("Choose between male and female");
+        }
+        else{
+            break;
+        }
+    }
+    var kjønn = kjønnsTest.toLowerCase();
+    for(var x = 1; x != 0; x++){
+        if(kjønn != "male" && kjønn != "female"){
+            alert("Please choose a valid sex...");
+            kjønnsTest = prompt("Choose between male and female");
+            kjønn = kjønnsTest.toLowerCase();
+        }
+        else{
+            break;
+        }
+    }
+
+
+
+    //Race
+    var raseTest = prompt("Choose between: Human, Elf, Orc or Halfling");
+    for(var x = 1; x != 0; x++){
+        if(raseTest === undefined){
+            alert("Please choose a valid race!");
+            raseTest = prompt("Choose between: Human, Elf, Orc or Halfling");
+        }
+        else{
+            break;
+        }
+    }
+
+    var rase = raseTest.toLowerCase();
+    for(var x = 1; x != 0; x++){
+        if(rase === "human" || rase === "elf" || rase === "orc" || rase === "halfling"){
+            break;
+        }
+        else{
+            alert("Please choose a valid race!");
+            raseTest = prompt("Choose between: Human, Elf, Orc or Halfling");
+            rase = raseTest.toLowerCase();
+        }
+    }
+
+
+
+
+    //Biggify first letter
+    function førsteBokstavStor(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }    
+
+    //start the game
+    alert("You have chosen: \"" + navn +"\" as your name, " + "\"" + førsteBokstavStor(kjønn) + "\" as your gender and \"" + førsteBokstavStor(rase) + "\" as your race! Now let the adventure begin!");
+
+
+        //figures out your race 
+                if(rase === "human"){
+                    race = human;
+
+                }
+                else if(rase === "orc"){
+                    race = orc;
+                }
+                else if(rase === "elf"){
+                    race = elf;
+
+                }
+                else if(rase === "halfling"){
+                    race = halfling;
+
+                }
+                else {
+                    alert("This is never supposed to happen. What did you do?")
+                }
+                
+                //Sets starter equipment based on race
+                currentWeapon = race.starterWeapon;
+                currentHat = race.starterHat;
+                currentShirt = race.starterShirt;
+                currentPants = race.starterPants;
+                currentBoots = race.starterBoots;
+                
+                //Sets HP and MP based on race
+                maxHP = race.HPBase + level;
+                maxMP = race.MPBase + level;
+                currentHP = maxHP;
+                currentMP = maxMP;
+                
+                //Displays name, sex and race
+                Navn.innerHTML = "Name: " + navn + " | ";
+                Kjønn.innerHTML = "Sex: " + førsteBokstavStor(kjønn) + " | ";
+                Rase.innerHTML = "Race: " + førsteBokstavStor(rase);
+                
+                //Displays inventory
+                inventory.innerHTML =  
+                    "<b>Inventory</b>" + 
+                    "<p>" + currentHat.name + "</p>" + 
+                    "<p>" + currentShirt.name + "</p>" + 
+                    "<p>" + currentWeapon.name + "/dmg: " + currentWeapon.DMG + "</p>" + 
+                    "<p>" + currentPants.name + "</p>" + 
+                    "<p>" + currentBoots.name +"</p>";
+                abilities.innerHTML = "<p>" + race.abilities.name + "</p>";
+                
+                //Displays HP and MP
+                HP.innerHTML = "HP: " + currentHP + "/" + maxHP + " | ";
+                MP.innerHTML = "MP: " + currentMP + "/" + maxMP;
+                
+            }
---
+++ variables.js
@@ -0,0 +1,262 @@
+//Original code   Arthur
+//changes by Simen
+
+    //Level
+var level = 1;
+
+    //HP and MP
+var currentHP;
+var maxHP;
+
+var currentMP;
+var maxMP;
+
+
+// An object that represents an empty equipment slot
+var empty = {
+    name: "empty",
+    type: "empty",
+    DMG: 0,
+    DEF: 0
+};
+
+
+var currentWeapon = empty;
+var currentHat = empty;
+var currentShirt = empty;
+var currentPants = empty;
+var currentBoots = empty;
+
+
+ /*Weapon template, COPY THIS!
+
+var  = {
+    name: "",
+    type: "",
+    DMG:  
+};
+*/
+
+    //starter weapons
+
+//Human starter weapon
+var kitchenKnife = {
+    name: "Kitchen Knife",
+    type: "knife",
+    DMG: 2
+};
+
+//Elf starter weapon
+var stick = {
+    name: "Stick",
+    type: "staff",
+    DMG: 1
+};
+
+//Halfling starter weapon
+var oldDagger = {
+    name: "Old dagger",
+    type: "knife",
+    DMG: 3
+};
+
+//Orc starting weapon
+var woodenClub = {
+    name: "Wooden club",
+    type: "club",
+    DMG: 3
+};
+
+    //Other wepaons
+var butcherKnife = {
+    name: "Buther knife",
+    type: "knife",
+    DMG: 4
+};
+
+var staff = {
+    name: "Staff",
+    type: "staff",
+    DMG: 3
+};
+
+var ironDagger = {
+    name: "Iron dagger",
+    type: "knife",
+    DMG: 5
+};
+
+var stoneClub = {
+    name: "Stoneclub",
+    type: "club",
+    DMG: 5
+};
+
+    //Hats
+/*Hat template COPY THIS!
+
+var = {
+    name:""
+};
+*/
+
+var strawHat = {
+    name: "Straw Hat"
+};
+
+var chainmailHood = {
+    name: "Chainmail hood"
+};
+
+var leatherHood = {
+    name: "Leather hood"
+};
+
+    //Shirts
+
+/*Shirt template COPY THIS!
+
+var = {
+    name: "",
+    type: ""
+};
+
+*/
+var woolenShirt = {
+    name: "Woolen shirt",
+    type: "shirt"
+};
+
+var chainmailShirt = {
+    name: "Chainmail shirt",
+    type: "shirt"
+};
+
+var clothRobe = {
+    name: "Cloth robe",
+    type: "robe"
+};
+
+var darkLeatherShirt = {
+    name: "Dark leather shirt",
+    type: "shirt"
+};
+
+    //Pants
+
+/* Pants template
+var = {
+    name: "",
+    type ""
+};
+*/
+
+var woolenPants = {
+    name: "Woolen pants",
+    type: "pants"
+};
+
+var wornLeatherPants = {
+    name: "Worn leather pants",
+    type: "pants"
+};
+
+var darkLeatherPants = {
+    name: "Dark leather pants",
+    type: "pants"
+};
+
+    //Boots
+
+/* Boots template
+var = {
+    name: "",
+    type: ""
+};
+*/
+
+var leatherBoots = {
+    name: "Leather boots",
+    type: "boots"
+};
+
+var clothBoots = {
+    name: "Cloth boots",
+    type: "boots"
+};
+
+
+    //Abilities
+
+/*
+//Human
+var selfHeal =  4 + level;
+var selfHealMP = 25;
+*/
+var fireball = {
+    name: "fireball",
+    MPcost: 25
+}
+/*Elf
+var fireball = currentWeapon.DMG + 4 + level;
+var fireballMP = 25;
+
+//Halfling
+var damageUp = currentWeapon.DMG + 1 + level;
+var damageUpMP = 25;
+
+//Orc
+var berserk = currentWeapon.DMG * 2 + level;
+var berserkMP = 25;
+*/
+    
+
+
+    
+    //Races
+var race;
+
+var human = {
+    HPBase: 99,
+    MPBase: 99,
+    starterWeapon: kitchenKnife,
+    starterHat: strawHat,
+    starterShirt: woolenShirt,
+    starterPants: woolenPants,
+    starterBoots: leatherBoots,
+   // abilities: selfHeal
+};
+
+var orc = {
+    HPBase: 199,
+    MPBase: 49,
+    starterWeapon: woodenClub,
+    starterHat: chainmailHood,
+    starterShirt: chainmailShirt,
+    starterPants: wornLeatherPants,
+    starterBoots: leatherBoots,
+   // abilities: berserk
+};
+
+var elf = {
+    HPBase: 74,
+    MPBase: 149,
+    starterWeapon: stick,
+    starterHat: empty,
+    starterShirt: clothRobe,
+    starterPants: empty,
+    starterBoots: clothBoots,
+    abilities: fireball
+};
+
+var halfling = {
+    HPBase: 99,
+    MPBase: 74,
+    starterWeapon: oldDagger,
+    starterHat: leatherHood,
+    starterShirt: darkLeatherShirt,
+    starterPants: darkLeatherPants,
+    starterBoots: leatherBoots,
+   // abilities: damageUp
+};
+
